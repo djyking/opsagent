597133246f59db007fe1327ceaa51cc1c40304ef
heyu <47731155+djyking@users.noreply.github.com>
2026-09-03 12:07:52 +0800

    进行联调与测试

--- ops-web/vite.config.ts
+++ ops-web/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    open: true,
+    open: false,
     proxy: {
       "/api": { target: "http://localhost:8080", changeOrigin: true },
       "/actuator": { target: "http://localhost:8080", changeOrigin: true },
--- sql/init_data.sql
+++ sql/init_data.sql
@@ -2,9 +2,9 @@ USE ops_auth;
 INSERT INTO sys_role(id,code,name,status,deleted) VALUES(1,'ADMIN','管理员','enable',0),(2,'OPS','运维人员','enable',0),(3,'USER','普通用户','enable',0) ON DUPLICATE KEY UPDATE name=VALUES(name),status=VALUES(status),deleted=0;
 INSERT INTO sys_permission(id,code,name,type,path,method,status) VALUES(1,'ticket:read','查看工单','API','/api/tickets/**','GET','enable'),(2,'ticket:operate','处理工单','API','/api/tickets/**','POST','enable'),(3,'knowledge:manage','知识库管理','API','/api/knowledge/**','ALL','enable'),(4,'platform:admin','平台管理','API','/api/platform/**','ALL','enable') ON DUPLICATE KEY UPDATE name=VALUES(name),status='enable';
 INSERT INTO sys_user(id,username,password,display_name,status,deleted) VALUES
-(1,'admin','$2a$10$3R0PLn3EPrcr3JmIWz61M./ea71Q7Z849ICq0sKienFN0SWQ3rzde','系统管理员','enable',0),
-(2,'ops','$2a$10$d.yy0sNJpCM1XA7hSJxG0evNqKUZpUzd2v2faPiqAR80ht0CTj.ee','运维人员','enable',0),
-(3,'user','$2a$10$d1mFiRIhZZFozM6MfYYoXePkXR1tCBxYt/YyO5s.1cn1oHDolGRQS','普通用户','enable',0)
+(1,'admin','$2a$10$2K0YuJta8zWfXR4SQPX8LubohC4pnFk1Smn4XEof4xeHdMXghsV5m','系统管理员','enable',0),
+(2,'ops','$2a$10$TpKqdWswnwVvFz/nkUvvaebCc8e6zoEPqV32eh6fec6YUFLoXx9hi','运维人员','enable',0),
+(3,'user','$2a$10$TX57ZL7yUst.7h06IHgavu7SgpcQzIrYgFYG0lbjJU8vJZQE5aNPC','普通用户','enable',0)
 ON DUPLICATE KEY UPDATE password=VALUES(password),display_name=VALUES(display_name),status='enable',deleted=0;
 INSERT IGNORE INTO sys_user_role(user_id,role_id) VALUES(1,1),(2,2),(3,3);
 INSERT IGNORE INTO sys_role_permission(role_id,permission_id) VALUES(1,1),(1,2),(1,3),(1,4),(2,1),(2,2),(2,3),(3,1);
